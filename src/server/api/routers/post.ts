df2ac5a4df448ee3c9ad61855bff55df3b3fad51
Radseq <radek.g.10@gmail.com>
2024-02-02 15:05:28 +0100

    feat: add posting post limit

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,10 @@
+import { TRPCError } from "@trpc/server";
+import { CreateRateLimit } from "RateLimit";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const postRateLimit = CreateRateLimit({ requestCount: 1, requestCountPer: "1 m" })
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -14,6 +18,13 @@ export const postRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
+
+      const { success } = await postRateLimit.limit("Create Post")
+
+			if (!success) {
+				throw new TRPCError({ code: "TOO_MANY_REQUESTS" })
+			}
+
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
