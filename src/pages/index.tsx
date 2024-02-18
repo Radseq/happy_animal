87b928b246e4edd6ef29e145716df5dd00cc8256
Radseq <radek.g.10@gmail.com>
2024-02-18 20:01:25 +0100

    fix: delete new line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Layout>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 lg:grid-cols-3">
-
-				</div>
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 lg:grid-cols-3"></div>
 			</Layout>
 		</>
 	)
